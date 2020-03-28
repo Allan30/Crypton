eed0b096ff9ff1a2a34a9ef44d43cfd298cd4c15
Des Courtils <allanthekiller@gmail.com>
2020-03-28 13:57:59 +0100

    Creation du projet Crypton

---
+++ Crypton/src/main/CryptonLauncher.java
@@ -0,0 +1,20 @@
+package main;
+
+/**
+ * The launcher of Crypton application
+ * @author allan
+ *
+ */
+public class CryptonLauncher {
+
+	/**
+	 * Launches the application
+	 * @param args command-line arguments (unused)
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world");
+		
+	}
+
+}
